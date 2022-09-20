7207bb8f54430bb3f89a00535e346dd51f5723a0
dblodorn <dain@db13.us>
2022-09-20 08:34:35 -0700

    WIP: dropping a vanilla react app in for non SSR testing

---
+++ examples/react/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+
+const domContainer = document.getElementById('main');
+const root = ReactDOM.createRoot(domContainer);
+root.render(<div>Hello</div>);

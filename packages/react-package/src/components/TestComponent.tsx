b57df61194a89915db38aea1a14f8871efb4970b
dblodorn <dain@db13.us>
2022-09-23 17:10:41 -0400

    test ts error

--- packages/react-package/src/components/TestComponent.tsx
+++ packages/react-package/src/components/TestComponent.tsx
@@ -1,4 +1,4 @@
-export function TestComponent({ text }: { text?: string }) {
+export function TestComponent({ text }: { text: string }) {
   return (
     <div className="rounded-xl border border-solid border-gray-200 p-4">
       This is a simple react component imported as an external package.{text}

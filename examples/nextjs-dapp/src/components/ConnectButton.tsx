57f36868f11a01eb4f6480851b597e70334d23e3
dblodorn <dain@db13.us>
2022-09-23 12:49:10 -0400

    update connect button and and avatar components in nextjs app

--- examples/nextjs-dapp/src/components/ConnectButton.tsx
+++ examples/nextjs-dapp/src/components/ConnectButton.tsx
@@ -4,7 +4,7 @@ import { Avatar } from './Avatar'
 export function ConnectButton({ ...props }) {
   return (
     <div
-      className="connect-button-wrapper relative flex items-center px-4 py-1.5 rounded-2xl bg-slate-100 overflow-hidden"
+      className="connect-button-wrapper relative flex items-center pr-3 pl-3 py-1.5 rounded-2xl bg-slate-100 overflow-hidden"
       {...props}
     >
       <RKConnectButton.Custom>
@@ -13,11 +13,14 @@ export function ConnectButton({ ...props }) {
             <>
               {(() => {
                 if (!mounted || !account || !chain) {
-                  return <button className="text-sm" onClick={openConnectModal}>Connect Wallet</button>
+                  return <button onClick={openConnectModal} className="text-sm">Connect Wallet</button>
+                }
+                if (chain.unsupported) {
+                  return <div className="text-red-400 text-sm">&#x26A0; Wrong Network</div>
                 }
                 return (
                   <button onClick={openAccountModal}>
-                    <div className="flex items-center gap-2">
+                    <div className="flex items-center gap-2 text-sm">
                       <Avatar />
                       {account.displayName}
                     </div>

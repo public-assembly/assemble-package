960822ad681c8373e49ae916ed93a339efb51a8d
dblodorn <dain@db13.us>
2022-09-25 18:57:37 -0400

    strictNullChecks false for tsc-files

--- examples/reactjs-dapp/src/index.tsx
+++ examples/reactjs-dapp/src/index.tsx
@@ -4,6 +4,7 @@ import { ExampleImports } from './components/example/ExampleImports'
 
 const domContainer = document.getElementById('main')
 const root = ReactDOM.createRoot(domContainer)
+
 root.render(
   <App>
     <ExampleImports />

f8a118ebc8c03cb2488538962c2e041f7b9d8a7e
dblodorn <dain@db13.us>
2022-11-28 17:31:04 -0500

    remove sample react app

--- examples/reactjs-dapp/src/components/RawDisplayer.tsx
+++
@@ -1,9 +0,0 @@
-export function RawDisplayer({ data }: { data: any }) {
-  return (
-    <div className="raw-displayer relative w-full overflow-x-scroll rounded-xl bg-gray-200 px-5 py-3 text-left">
-      <code>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </code>
-    </div>
-  )
-}
--- examples/reactjs-dapp/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './.{html,js,ts,jsx,tsx}',
-    './**/*.{html,js,ts,jsx,tsx}',
-    './../../examples/**/*.{js,ts,jsx,tsx}',
-    './../../packages/**/*.{js,ts,jsx,tsx}',
-  ],
-}

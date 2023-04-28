1b84259ced1199a3637a47036e41210f9f2c3c8b
Salief <salieflewis@gmail.com>
2023-04-28 15:48:55 -0400

    add formatter config

--- apps/next/pages/examples.tsx
+++ apps/next/pages/examples.tsx
@@ -3,22 +3,22 @@ import dynamic from 'next/dynamic'
 import { Seo } from '@/components/Seo'
 
 const DynamicComponent = dynamic(
-	() => import('../components/example/DynamicComponent'),
-	{
-		ssr: false,
-	},
+  () => import('../components/example/DynamicComponent'),
+  {
+    ssr: false,
+  },
 )
 
 function Examples() {
-	const { address } = useAccount()
+  const { address } = useAccount()
 
-	return (
-		<section className="max-w-[1240px] m-auto px-4 gap-8 flex flex-col">
-			<Seo title="examples" />
-			<h1>Connect your wallet to see the magic 🪄</h1>
-			{address ? <DynamicComponent address={address} /> : null}
-		</section>
-	)
+  return (
+    <section className="max-w-[1240px] m-auto px-4 gap-8 flex flex-col">
+      <Seo title="examples" />
+      <h1>Connect your wallet to see the magic 🪄</h1>
+      {address ? <DynamicComponent address={address} /> : null}
+    </section>
+  )
 }
 
 export default Examples

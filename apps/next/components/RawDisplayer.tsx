cedeb10db3323d808fdee5745955f3afa4db661c
Salief <salieflewis@gmail.com>
2023-07-25 19:13:45 +0200

    strip old webpack deps

--- apps/next/components/RawDisplayer.tsx
+++ apps/next/components/RawDisplayer.tsx
@@ -1,3 +1,4 @@
+// rome-ignore lint: Can't predict data type
 export function RawDisplayer({ data }: { data: any }) {
   return (
     <div className="raw-displayer relative w-full overflow-x-scroll rounded-xl bg-gray-200 px-5 py-3 text-left">

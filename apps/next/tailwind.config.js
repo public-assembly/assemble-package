794f786647de81f864ce5635187c8a08ee76f481
Salief <salieflewis@gmail.com>
2023-07-25 17:50:31 +0200

    push sandbox up to latest deps

--- apps/next/tailwind.config.js
+++ apps/next/tailwind.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx, md, mdx}',
+    './app/**/*.{js,ts,jsx,tsx, md, mdx}',
     './components/**/*.{js,ts,jsx,tsx, md, mdx}',
     './../../packages/**/*.{js,ts,jsx,tsx, md, mdx}',
   ],
